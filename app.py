b6babf68378155da07e4bf852ee361b9639869bc
Xiemanman <44190812+Xiemanman@users.noreply.github.com>
2019-03-30 18:13:15 +1100

    Update app.py

--- app.py
+++ app.py
@@ -14,28 +14,24 @@ import IntentClassification as intent_classify
 import keyword_extraction as keyword_extract
 import retrieve_data as retrieve
 
-'''
-client = boto3(
-    'dynamodb',
-    aws_access_key_id = os.environ['aws-accesss-id'],
-    aws_secret_access_key = os.environ['aws-access-key'])
-'''
-
-#greeting = ['hello','hi','how are you','nice to meet you']
-
-#goodbye = ['bye','goodbye','see you','thank you']
-
+#import time
 
 
 facebook_verify = os.environ['facebook_verify'] #'we'   #the verify token
 ## may need change everytime
-access_token = os.environ['access_token'] #'EAADkSAAGGFcBANBEhgqBQ1nV1obaMJ5iGquSbZA6kHQh28vJjrkiKhgwjwQ67DlqQ4hTiZBSfNpiuBRsYJmYTZCrxZBkt9NU8vZCZCtVMZAhigS3UsSIfgmqTB7Vu0DhNqCEQOcqApZBHWbrcvjqCwCXZAJAEeDN5DK12kEvfUeTk7QZDZD'    #the access token
+access_token = os.environ['access_token']    #the access token
 server = Bot(access_token)
 
 global re_ask
 re_ask = False
 global re_intent
 re_intent = ''
+
+'''
+# store the key info by user id
+global store
+store = {}   # store = {'id':{'re_intent':'','keyword':{}, 're_ask': Flase, 'time': float}}
+'''
 
 
 app = Flask(__name__)
@@ -59,6 +55,7 @@ def verify_facebook():
 def recieve_message():
     global re_ask
     global re_intent
+    #global store
     user_input = request.get_json()
     #get user ID to response back
     for event in user_input['entry']:
@@ -68,6 +65,58 @@ def recieve_message():
                 #Facebook Messenger ID for user so we know where to send response back to
                 text = message['message'].get('text')
                 user_ID = message['sender']['id']
+                '''
+                sent_time = time.time()   ## current time
+                if user_ID not in store.keys():
+                    store[user_ID] = {'re_intent':'', 'keyword':{}, 're_ask': Flase, 'time': sent_time}
+                else:
+                    break_time = sent_time - store[user_ID]['time']
+                    if break_time > 120:  # longer than 2 mins
+                        store[user_ID] = {'intent':'','keyword':{}, 're_ask': Flase, 'time': sent_time}
+                    else:
+                        store[user_ID]['time'] = sent_time
+                
+                if store[user_ID]['re_ask'] == False:
+                    new_text = TextBlob(text).correct()
+                    new_text = str(new_text)
+                    intent = intent_classify.intent_classification(new_text)
+                    
+                    if intent == 'Greetings':
+                        response = 'Hi, I am here to help you!'
+                        reply_user(user_ID,response)
+                        return "Message Processed"
+                    elif intent == 'Goodbye':
+                        response = 'See you soon!'
+                        reply_user(user_ID,response)
+                        return "Message Processed"
+                else:
+                    intent = store[user_ID]['re_intent']
+                keyword = keyword_extract.keyword_extraction(intent,text)
+                response = retrieve.retrieval_func(keyword)
+                if response == 'Please provide courses code.':
+                    if store[user_ID]['keyword']!={}:
+                        if store[user_ID]['keyword']['course'] != []:
+                            store[user_ID]['keyword']['intent'] = intent
+                            response = retrieve.retrieval_func(store[user_ID]['keyword'])
+                    else:
+                        store[user_ID]['re_ask'] = True
+                        store[user_ID]['re_intent'] = intent
+                elif response == 'please provide stream name.':
+                    if store[user_ID]['keyword']!={}:
+                        if store[user_ID]['keyword']['stream_name'] != []:
+                            store[user_ID]['keyword']['intent'] = intent
+                            response = retrieve.retrieval_func(store['user_ID]['keyword'])
+                    else:
+                        store[user_ID]['re_ask'] = True
+                        store[user_ID]['re_intent'] = intent
+                else:
+                    store[user_ID]['re_ask'] = False
+                    store[user_ID]['re_intent'] = ''
+                
+                res = intent + ' ' + response + ' ' + str(store[user_ID]['re_ask'])
+                
+                reply_user(user_ID,res)
+                '''
                 
                 if re_ask == False:
                     new_text = TextBlob(text).correct()
@@ -120,18 +169,6 @@ def reply_user(user_ID,message):
     server.send_text_message(user_ID,message)
     return 'ok'
 
-'''
-# test dynamodb
-def test_get_dynamodb():
-# Connect to aws Dynamobd, get required data
-    dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2')
-    table = dynamodb.Table('Monster_DB')
-
-    response = table.get_item(Key={'Function': 'Basic_courses_information', 'Course': 'COMP9900'}, AttributesToGet=['handbook_link'])
-    print(response)
-    link = response['Item']['handbook_link']
-    return link
-'''
 
 if __name__ == '__main__':
     #print(test_get_dynamodb())
