09e8af6b18aae3351282f9ffc0b91e461029f1a2
Xiemanman <44190812+Xiemanman@users.noreply.github.com>
2019-04-19 16:01:59 +1000

    Update feedback.py

--- feedback.py
+++ feedback.py
@@ -6,5 +6,4 @@ dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2')
 table = dynamodb.Table('Monster_DB')
 
 def feedback(user_id, user_input, intent):
-
-	return table.put_item(Item = {'Function': 'Feedback', 'Course': user_id, 'input': user_input, 'intent': intent})
+	table.put_item(Item = {'Function': 'Feedback', 'Course': user_id, 'input': user_input, 'intent': intent})
